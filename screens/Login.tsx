52247c878a9ff2a220b9d4e03658f1381263042f
Eumericano <opwer032@naver.com>
2022-02-20 14:01:24 +0900

    feat: RN에서 react hook form 사용

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,4 +1,5 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
+import { useForm } from "react-hook-form";
 import { TextInput } from "react-native";
 
 import AuthButton from "../components/auth/AuthButton";
@@ -6,12 +7,21 @@ import AuthLayout from "../components/auth/AuthLayout";
 import { StyledTextInput } from "../components/auth/AuthShared";
 
 export default function LogIn() {
+  const { register, handleSubmit, setValue } = useForm();
+
   const passwordRef = useRef<TextInput>(null);
 
   const onNext = (nextOne: React.RefObject<TextInput>) =>
     nextOne?.current?.focus();
 
-  const onDone = () => alert("done");
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+
+  useEffect(() => {
+    register("Username");
+    register("Password");
+  }, [register]);
 
   return (
     <AuthLayout>
@@ -21,6 +31,7 @@ export default function LogIn() {
         placeholderTextColor="rgba(255,255,255,0.8)"
         returnKeyType="next"
         onSubmitEditing={() => onNext(passwordRef)}
+        onChangeText={(text: string) => setValue("Username", text)}
       />
       <StyledTextInput
         ref={passwordRef}
@@ -29,9 +40,14 @@ export default function LogIn() {
         secureTextEntry
         returnKeyType="done"
         lastOne={true}
-        onSubmitEditing={onDone}
+        onSubmitEditing={handleSubmit(onValid)}
+        onChangeText={(text: string) => setValue("Password", text)}
       />
-      <AuthButton text="Create Account" disabled={true} onPress={() => null} />
+      <AuthButton
+        text="Create Account"
+        disabled={true}
+        onPress={handleSubmit(onValid)}
+      />
     </AuthLayout>
   );
 }
