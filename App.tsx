882477475de3fa997a97610ea822930bd7f662d1
Eumericano <opwer032@naver.com>
2022-02-21 01:01:52 +0900

    feat: apollo 초기설정

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
-
 import AppLoading from "expo-app-loading";
 import * as Font from "expo-font";
 import { Asset } from "expo-asset";
-import LoggedOutNav from "./navigators/LoggedOutNav";
 import { NavigationContainer } from "@react-navigation/native";
+
+import { ApolloProvider } from "@apollo/client";
+import client from "./apollo";
+import LoggedOutNav from "./navigators/LoggedOutNav";
 
 export default function App() {
   const [loading, setLoading] = useState<boolean>(true);
@@ -30,8 +32,10 @@ export default function App() {
     );
 
   return (
-    <NavigationContainer>
-      <LoggedOutNav />
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <LoggedOutNav />
+      </NavigationContainer>
+    </ApolloProvider>
   );
 }
