3f6790aab5dc9dd77309245be7736f5e098c42e7
Eumericano <opwer032@naver.com>
2022-02-22 21:54:49 +0900

    style: tab navigator 스타일 추가

--- navigators/LoggedInNav.tsx
+++ navigators/LoggedInNav.tsx
@@ -1,12 +1,68 @@
+import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import { Ionicons } from "@expo/vector-icons";
+
 import Feed from "../screens/Feed";
+import Notification from "../screens/Notification";
+import Profile from "../screens/Profile";
+import Search from "../screens/Search";
 
 const Tabs = createBottomTabNavigator();
 
 export default function LoggedInNav() {
   return (
-    <Tabs.Navigator screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="Feed" component={Feed} />
+    <Tabs.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "white",
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          borderTopColor: "rgba(255,255,255,0.2)",
+          backgroundColor: "black",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="Feed"
+        component={Feed}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name="home" color={color} size={focused ? 24 : 22} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name="search" color={color} size={focused ? 24 : 22} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Notification"
+        component={Notification}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name="heart-outline"
+              color={color}
+              size={focused ? 24 : 22}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name="person" color={color} size={focused ? 24 : 22} />
+          ),
+        }}
+      />
     </Tabs.Navigator>
   );
 }
--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,9 +1,9 @@
 import { Text, View } from "react-native";
 
-export default function Feed() {
+export default function Search() {
   return (
     <View style={{ backgroundColor: "black", flex: 1 }}>
-      <Text style={{ color: "white" }}>Hello</Text>
+      <Text style={{ color: "white" }}>Search</Text>
     </View>
   );
 }
